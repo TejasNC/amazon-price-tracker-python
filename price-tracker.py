6e335316d8d3e7802e7ab763514e3cc557ce4904
Tejas <tejashns169@gmail.com>
2024-09-30 16:36:34 +0530

    Added support to latest version of amazon.in

--- price-tracker.py
+++ price-tracker.py
@@ -1,17 +1,21 @@
 # import required files and modules
-
 import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
+import os
+import json
 
-# set the headers and user string
+#Open the params.json file
+with open('params.json','r') as file:
+    params = json.load(file)
+    
 headers = {
-"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+    "User-Agent": params['User-Agent']
 }
 
 # send a request to fetch HTML of the page
-response = requests.get('https://www.amazon.in/Bose-SoundLink-Wireless-Around-Ear-Headphones/dp/B0117RGG8E/ref=sr_1_11?qid=1562395272&refinements=p_89%3ABose&s=electronics&sr=1-11', headers=headers)
+response = requests.get(params["URL"], headers=headers)
 
 # create the soup object
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -22,48 +26,94 @@ soup.encode('utf-8')
 #print(soup.prettify())
 
 # function to check if the price has dropped below 20,000
-def check_price():
+def check_price(prev_price=0):
   title = soup.find(id= "productTitle").get_text()
-  price = soup.find(id = "priceblock_ourprice").get_text().replace(',', '').replace('₹', '').replace(' ', '').strip()
+  price = soup.find(class_ = "a-price-whole").get_text().replace(',', '').replace('₹', '').replace(' ', '').strip()
   #print(price)
 
   #converting the string amount to float
   converted_price = float(price[0:5])
   print(converted_price)
-  if(converted_price < 20000):
-    send_mail()
-
+  
+  if(converted_price < params['Target_Price']):
+    send_mail(converted_price, below_budget=True)
+  else:
+    # print('Price is still high')
+    if prev_price==0:
+      send_mail(converted_price, below_budget=False)
+    else:
+      diff = prev_price - converted_price #if price has risen, then the diff will be negative
+      send_mail(diff, below_budget=False)
   #using strip to remove extra spaces in the title
+  prev_price = converted_price
   print(title.strip())
 
 
 
 
 # function that sends an email if the prices fell down
-def send_mail():
-  server = smtplib.SMTP('smtp.gmail.com', 587)
-  server.ehlo()
-  server.starttls()
-  server.ehlo()
-
-  server.login('email@gmail.com', 'password')
-
-  subject = 'Price Fell Down'
-  body = "Check the amazon link https://www.amazon.in/Bose-SoundLink-Wireless-Around-Ear-Headphones/dp/B0117RGG8E/ref=sr_1_11?qid=1562395272&refinements=p_89%3ABose&s=electronics&sr=1-11 "
-
-  msg = f"Subject: {subject}\n\n{body}"
+def send_mail(diff,below_budget=True):
   
-  server.sendmail(
-    'sender@gmail.com',
-    'receiver@gmail.com',
-    msg
-  )
-  #print a message to check if the email has been sent
-  print('Hey Email has been sent')
-  # quit the server
-  server.quit()
+  if below_budget:
+    
+    server = smtplib.SMTP('smtp.gmail.com', 587)    
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
 
+    server.login(params["Sender_Email"], params["Sender_Email_Password"])   #enter sender email id and sender email id password
+
+    subject = 'Price Has Fallen Down Below Target!'
+    URL = params["URL"]
+    body = f"Check the amazon link {URL}"
+
+    msg = f"Subject: {subject}\n\n{body}"
+    
+    server.sendmail(
+      params["Sender_Email"],   # enter sender email id
+      params["Reciever_Email"], # enter receiver email id
+      msg                   # message that is to be sent
+    )
+    #print a message to check if the email has been sent
+    print('Hey Email has been sent')
+    # quit the server
+    server.quit()
+
+  if not below_budget:
+
+    if (diff<0):
+      subject = f'Price Has Risen By {(-1*diff)}!'
+      URL = params["URL"]
+      body = f"Check the amazon link {URL}"
+    elif(diff>0):
+      subject = f'Price Has Fallen By {diff}!'
+      URL = params["URL"]
+      body = f"Check the amazon link {URL}"   
+    else:
+      return 
+        
+    print('Price is still higher than Target Price')
+    server = smtplib.SMTP('smtp.gmail.com', 587)    
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login(params["Sender_Email"], params["Sender_Email_Password"])   #enter sender email id and sender email id password
+      
+    msg = f"Subject: {subject}\n\n{body}"
+    
+    server.sendmail(
+      params["Sender_Email"],   # enter sender email id
+      params["Receiver_Email"], # enter receiver email id
+      msg                   # message that is to be sent
+    )
+    #print a message to check if the email has been sent
+    print('Hey Email has been sent')
+    # quit the server
+    server.quit()
+    
 #loop that allows the program to regularly check for prices
+prev_price = 0 
 while(True):
-  check_price()
-  time.sleep(60 * 60)
+  check_price(prev_price)
+  time.sleep(params['Time_Interval']) # the time after which you want the program to check the price in seconds. It is currently set to 1 day
